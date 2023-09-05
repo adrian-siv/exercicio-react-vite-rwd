ad08c78750a78b6424b90905506ab8a3799f037d
adrian-siv <adrian.ssiv@gmail.com>
2023-09-04 22:02:47 -0300

    Criado e renderizado componente "Menu"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import Menu from "./components/Menu";
 Outlet;
 
 function App() {
 	return (
 		<>
+			<Menu />
 			<Outlet />
 		</>
 	);
